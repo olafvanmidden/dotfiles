30c25cd4169a3bb7877b05b913e16c2e951a10e3
Olaf van Midden SB <olafvanmidden@socialbrothers.nl>
2026-01-19 20:16:42 +0100

    Add .config/nvim/lua/plugins/telescope.lua

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+        },
+      },
+    },
+  },
+}
