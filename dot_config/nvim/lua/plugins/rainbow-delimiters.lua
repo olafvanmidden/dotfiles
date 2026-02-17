b9db0cb2fdd42b5f21c1cee5271e35b8998088c5
Olaf van Midden SB <olafvanmidden@socialbrothers.nl>
2026-02-17 13:49:28 +0100

    Fixed delimiters to match vscode / cursor plugin

--- dot_config/nvim/lua/plugins/rainbow-delimiters.lua
+++ dot_config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -2,5 +2,26 @@ return {
   {
     "HiPhish/rainbow-delimiters.nvim",
     event = "VeryLazy",
+    init = function()
+      ---@type rainbow_delimiters.config
+      vim.g.rainbow_delimiters = {
+        highlight = {
+          "RainbowDelimiter1",
+          "RainbowDelimiter2",
+          "RainbowDelimiter3",
+          "RainbowDelimiter4",
+        },
+      }
+
+      local function set_rainbow_hls()
+        vim.api.nvim_set_hl(0, "RainbowDelimiter1", { fg = "#E6B422" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiter2", { fg = "#C70067" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiter3", { fg = "#00a960" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiter4", { fg = "#FC7482" })
+      end
+
+      vim.api.nvim_create_autocmd("ColorScheme", { callback = set_rainbow_hls })
+      set_rainbow_hls()
+    end,
   },
 }
